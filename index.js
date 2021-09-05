6659f3a555370860b3fd59d7f23ef39f3d7fd0cb
FayozbekOlimov <Fayozbek2021dev@gmail.com>
2021-09-05 09:36:55 +0500

    Background changed and icon added

--- index.js
+++ index.js
@@ -7,22 +7,41 @@ githubCard.className = 'github-card';
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     githubCard.innerHTML = '';
+    
     const API = `https://api.github.com/users/${input.value}`;
     const API_REPO = API + '/repos?sort=created';
 
     fetch(API)
-        .then(response => response.json())
-        .then(getResult);
+        .then(
+            function response(res) {
+                if (res.status != 404) {
+                    return res.json();
+                } else {
+                    githubCard.innerHTML = 'This User Is Not Found !';
+                    githubCard.style.color = 'white';
+                    return;
+                }
+            }
+        )
+        .then(getResult)
 
     fetch(API_REPO)
-        .then(response => response.json())
+        .then(
+            function response(res) {
+                if (res.status != 404) {
+                    return res.json();
+                } else {
+                    return;
+                }
+            }
+        )
         .then(getRepos);
 });
 
 function getResult(data) {
     const githubData = document.createElement('div');
     githubData.className = 'github-data';
-    
+
     githubData.innerHTML = `
         <div class="github-logo">
             <img id="avatar" src=${data.avatar_url} alt="Avater">
@@ -41,16 +60,11 @@ function getResult(data) {
         </div>
     `;
     githubCard.appendChild(githubData);
-
-    // document.getElementById('avatar').src = `${data.avatar_url}`;
-    // document.getElementById('github-name').innerText = `${data.name}`;
-    // document.querySelector('.number-followers').innerText = `${data.followers}`;
-    // document.querySelector('.number-following').innerText = `${data.following}`;
 }
-function getRepos(data) {
-    console.log(data);
 
+function getRepos(data) {
     const githubRepos = document.createElement('div');
+
     githubRepos.className = 'github-repos';
     githubRepos.innerHTML = '<h2 class="label-repo">Some Repos</h2>';
 
@@ -58,18 +72,23 @@ function getRepos(data) {
     reposWrapper.className = 'repos-wrapper';
 
     let number = data.length > 6 ? 6 : data.length;
-    for(let i = 0; i < number; i++) {
-        const repo = document.createElement('div');
-        repo.className = 'repo';
-        repo.innerHTML = `
-            <a href=${data[i].svn_url} target="_blank" class="repo-name">${data[i].name}</a>
-            <div class="repo-features">
-                <span class="stars"><i class="fas fa-star"></i> ${data[i].stargazers_count}</span>
-                <span class="lang"><i class="fas fa-code"></i> ${data[i].language}</span>
-                <span class="size"><i class="fas fa-cloud-download-alt"></i> ${data[i].size}kb</span>
-            </div>
-        `;
-        reposWrapper.appendChild(repo);
+
+    if (number == 0) {
+        githubRepos.innerHTML = 'This user doesn\'t have any repositories yet';
+    } else {
+        for (let i = 0; i < number; i++) {
+            const repo = document.createElement('div');
+            repo.className = 'repo';
+            repo.innerHTML = `
+                <a href=${data[i].svn_url} target="_blank" class="repo-name">${data[i].name}</a>
+                <div class="repo-features">
+                    <span class="stars"><i class="fas fa-star"></i> ${data[i].stargazers_count}</span>
+                    <span class="lang"><i class="fas fa-code"></i> ${data[i].language}</span>
+                    <span class="size"><i class="fas fa-cloud-download-alt"></i> ${data[i].size}kb</span>
+                </div>
+            `;
+            reposWrapper.appendChild(repo);
+        }
     }
 
     githubRepos.appendChild(reposWrapper);
@@ -77,67 +96,3 @@ function getRepos(data) {
 }
 
 form.insertAdjacentElement('afterend', githubCard);
-
-
-
-
-// const form = document.getElementById('form');
-// const input = document.getElementById('search-input');
-
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const API = `https://api.github.com/users/${input.value}`;
-//     const API_REPO = API + '/repos?sort=created';
-//     document.querySelector('.github').classList.remove('hidden');
-
-//     fetch(API)
-//         .then(response => response.json())
-//         .then(getResult);
-
-//     fetch(API_REPO)
-//         .then(response => response.json())
-//         .then(getRepos);
-// });
-
-// function getResult(data) {
-//     // const github = document.createElement('div');
-//     // github.className = 'github';
-//     // github.innerHTML = `
-//     //     <div class="github-img">
-//     //         <img id="avatar" src=${data.avatar_url} alt="LOGO">
-//     //     </div>
-//     //     <div class="github-data">
-//     //         <h2 class="name">${data.name}</h2>
-//     //         <p class="bio">${data.bio}</p>
-//     //         <div class="numbers">
-//     //             <label class="followers">
-//     //                 <span id="number-followers">${data.followers}</span> Followers
-//     //             </label>
-//     //             <label class="following">
-//     //                 <span id="number-following">${data.following}</span> Following
-//     //             </label>
-//     //             <label class="repos">
-//     //                 <span id="number-repos">${data.length}</span> Repos
-//     //             </label>
-//     //         </div>
-//     //         <div class="repos"></div>
-//     //     </div>
-//     // `;
-
-//     document.getElementById('avatar').src = `${data.avatar_url}`;
-//     document.querySelector('.name').innerText = `${data.name}`;
-//     document.querySelector('.bio').innerText = `${data.bio}`;
-//     document.getElementById('number-followers').innerText = `${data.followers}`;
-//     document.getElementById('number-following').innerText = `${data.following}`;
-// }
-
-// function getRepos(data) {
-//     console.log(data);
-//     for(let i = 0; i < 5; i++) {
-//         const a = document.createElement('a');
-//         a.href = `${data[i].html_url}`;
-//         a.innerHTML = `${data[i].name}`;
-//         document.querySelector('.repos').appendChild(a);
-//     }
-// }
